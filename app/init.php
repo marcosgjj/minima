3fa92298e3e348b2870db0c06d0011a51f97d24e
Wolkadan99 <marcosgjdj@gmail.com>
2021-10-06 16:40:19 -0300

    Core build done

--- app/init.php
+++ app/init.php
@@ -1,6 +1,8 @@
 <?php
 
 //inicializador 
+require "../app/core/config.php";
 require "../app/core/functions.php";
+require "../app/core/database.php";
 require "../app/core/controller.php";
 require "../app/core/app.php";
